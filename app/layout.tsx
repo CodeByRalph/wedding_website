cb1b0a9c44b0734d315ddfc796424676ba816831
Ralph Stout <ralph.stout14@icloud.com>
2023-11-13 00:16:45 -0600

    added the naavbar for the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-green-400`}>
         {/* navbar goes here since its on all pages */}
-        <nav>
-          < HiMenu className="text-white text-5xl float-right mr-5 pt-3 sm:hidden"/>
-          <h1 className="text-white text-2xl ml-5 pt-3">Odette & Ralph</h1>
-          <ul className="hidden list-none sm:inline-block">
-            <li>Our Story</li>
-            <li>Info</li> 
-            <li>Registry</li>
-            <li>Sponsorship</li>
-            <li>RSVP</li>
+        <nav className="sm:flex ml-3 mr-3 mt-3">
+          < HiMenu className="text-white text-4xl float-right mr-5 sm:hidden"/>
+          <h1 className="text-white text-2xl sm:w-1/3">Odette & Ralph</h1>
+          <ul className="hidden list-none sm:flex flex-row justify-end items-center w-2/3 float-right text-white">
+            <li className="mr-3">Our Story</li>
+            <li className="mr-3">Info</li> 
+            <li className="mr-3">Registry</li>
+            <li className="mr-3">Sponsorship</li>
+            <li className="mr-3">RSVP</li>
           </ul>
         </nav>
         {children}
